f015c31750acfb176961e72a17b9eb05491c15d1
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-27 21:39:39 +0200

    retrofit implemented

--- app/src/main/java/cz/levinzonr/cryptostore/model/ExchangeRatesRemote.kt
+++ app/src/main/java/cz/levinzonr/cryptostore/model/ExchangeRatesRemote.kt
@@ -2,29 +2,52 @@ package cz.levinzonr.cryptostore.model
 
 import android.os.Handler
 import android.util.Log
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import rx.Observable
 import java.util.concurrent.TimeUnit
 
 class ExchangeRatesRemote {
 
-    companion object {
+    private val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
 
+    private val retrofit = Retrofit.Builder()
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    private val service = retrofit.create(CoinMarketCapService::class.java)
+
+    companion object {
+        const val BASE_URL = "https://api.coinmarketcap.com/v1/"
         const val TAG = "RatesRemote"
 
-         fun items(): ArrayList<Currency>{
-             val items = ArrayList<Currency>()
-             items.add(Currency("bitcoint", "Bitcoin", "BTC", 202031.1))
-             items.add(Currency("ethereum","Ethereum", "ETH", 12.1844))
-             return items
-         }
+        var instance : ExchangeRatesRemote? = null
+
+        fun instance() : ExchangeRatesRemote {
+            if (instance == null) {
+                instance = ExchangeRatesRemote()
+            }
+            return instance as ExchangeRatesRemote
+        }
 
     }
 
      fun geExchangeRates() : Observable<ArrayList<Currency>> {
         Log.d(TAG, "Start loading data...")
-         return Observable.just(ExchangeRatesLocal.items()).delay(2, TimeUnit.SECONDS)
+         return service.getExchangeRates()
     }
 
-
+    interface CoinMarketCapService {
+        @GET("ticker") fun getExchangeRates() : Observable<ArrayList<Currency>>
+    }
 
 }
--- app/src/main/java/cz/levinzonr/cryptostore/model/RatesRepo.kt
+++ app/src/main/java/cz/levinzonr/cryptostore/model/RatesRepo.kt
@@ -7,13 +7,12 @@ import rx.Observable
 class RatesRepo(context: Context) {
 
     val localData = ExchangeRatesLocal()
-    val remoteData = ExchangeRatesRemote()
     val netManager: NetworkManager = NetworkManager(context)
 
      fun geExchangeRates() : Observable<ArrayList<Currency>> {
 
          if (netManager.isConnectedToInternet()){
-             remoteData.geExchangeRates()
+             return ExchangeRatesRemote.instance().geExchangeRates()
          }
          return localData.geExchangeRates()
      }
